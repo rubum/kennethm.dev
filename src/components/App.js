f3514d51cb7aeb4a5a6558ef7a23ec71f0a1fe8d
Kenneth Mburu <mburunganga4@gmail.com>
2022-11-18 21:24:25 +0300

    Handle profile pop

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <Navbar />
-      <div className='app-body'>
+      <div className='app-body mx-auto max-w-5xl px-2 pt-4 sm:px-6 lg:px-8'>
         <h1 className=''>Hi!</h1>
         <p>Welcome to my simple site</p>
       </div>
